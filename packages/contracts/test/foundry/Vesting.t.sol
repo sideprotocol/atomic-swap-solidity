ee01b0d626b7b25b0cbc4991a505343673210221
DedicatedDev <topdev323@gmail.com>
2023-12-06 03:39:53 +0200

    feat: adding fuzzy testing

--- packages/contracts/test/foundry/Vesting.t.sol
+++ packages/contracts/test/foundry/Vesting.t.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 import "forge-std/Test.sol";
+import {InchainAtomicSwap} from "../../contracts/inchain_atomicswap/InchainAtomicSwap.sol";
+import {MockToken} from "../../contracts/mocks/MockToken.sol";
 import {Vesting} from "../../contracts/vesting/Vesting.sol";
 import {InchainAtomicSwap} from "../../contracts/inchain_atomicswap/InchainAtomicSwap.sol";
 import {IAtomicSwapBase} from "../../contracts/abstracts/interfaces/IAtomicSwapBase.sol";
@@ -10,14 +12,26 @@ import {TestHelper} from  "./utils/TestHelper.sol";
 contract VestingTest is Test {
     InchainAtomicSwap atomicswap;
     Vesting vesting;
+    MockToken usdc;
+    MockToken usdt;
     address admin = address(0x90);
+    address treasury = address(0x10);
+    uint256 sellerFee = 100;
+    uint256 buyerFee = 120;
+
+    address user1 = address(0x11);
+    address user2 = address(0x12);
+    address user3 = address(0x13);
     // Setup function runs before each test
     function setUp() public {
         // Deploy vesting contract
         vesting = new Vesting();
         bytes memory initData = abi.encodeWithSelector(
             vesting.initialize.selector,
-            admin
+            admin,
+            "vSide",
+            "vSide",
+            ""
         );
 
         address proxy = TestHelper.deployWithTP(
@@ -26,8 +40,37 @@ contract VestingTest is Test {
             initData
         );
         vesting = Vesting(payable(address(proxy)));
-    }
 
+
+         // Deploy atomicswap contract 
+        atomicswap = new InchainAtomicSwap();
+        bytes memory initAtomicSwapData = abi.encodeWithSelector(
+            atomicswap.initialize.selector,
+            admin,
+            address(vesting),
+            treasury,
+            sellerFee, 
+            buyerFee
+        );
+
+        address atomicSwapProxy = TestHelper.deployWithTP(
+            address(atomicswap),
+            admin,
+            initAtomicSwapData
+        );
+
+        atomicswap = InchainAtomicSwap(atomicSwapProxy);
+
+        usdc = new MockToken("USDC","USDC");
+        usdt = new MockToken("USDT","USDT");
+
+        vm.deal(user1, 100 ether); // Provide user1 with some ETH
+        vm.deal(user2, 100 ether); // Provide user2 with some ETH
+        vm.deal(user3, 100 ether); // Provide user2 with some ETH
+
+        usdc.mint(user2, 1000 ether); // Provide user1 with some
+        usdt.mint(user3, 1000 ether); // Provide user1 with some
+    }
     // Fuzz test for startVesting
     function testFuzz_startVesting(bytes32 orderId, address buyer, address token, uint256 totalAmount) public {
         IAtomicSwapBase.Release[] memory releases = new IAtomicSwapBase.Release[](1);
@@ -36,10 +79,39 @@ contract VestingTest is Test {
         vesting.startVesting(orderId, buyer, token, totalAmount, releases);
     }
 
-    // // Fuzz test for release
-    function testFuzz_release(uint vestingId) public {
-        // This will need additional setup to ensure there are vesting schedules to release
-        // vesting.release(vestingId);
-        // Add assertions or conditions here
-    }
+    // // // Fuzz test for release
+    // function testFuzz_release(
+    //     bytes32 uuid
+    // ) public {
+    //     IAtomicSwapBase.MakeSwapMsg memory makeswap = IAtomicSwapBase.MakeSwapMsg(
+    //         uuid,
+    //         IAtomicSwapBase.Coin(address(0), 10 ether), 
+    //         IAtomicSwapBase.Coin(address(usdt), 10 ether),
+    //         address(user1),
+    //         address (user2),
+    //         8 ether,
+    //         block.timestamp + 1000,
+    //         true
+    //     );
+
+    //     IAtomicSwapBase.Release[] memory releases = new IAtomicSwapBase.Release[](2);
+    //     releases[0] = IAtomicSwapBase.Release(
+    //         1 hours,
+    //         5000
+    //     );
+    //     releases[1] = IAtomicSwapBase.Release(
+    //         1 hours,
+    //         5000
+    //     );
+
+    //     // Create Order
+    //     vm.startPrank(user1);
+    //     atomicswap.makeSwapWithVesting(
+    //        makeswap,
+    //        releases
+    //     );
+    //     // This will need additional setup to ensure there are vesting schedules to release
+    //     // vesting.release(vestingId);
+    //     //Add assertions or conditions here
+    // }
 }
